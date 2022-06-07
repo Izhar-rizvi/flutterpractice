2b1dd3c77fa70e8afad2b29841d389f281ec1912
IzharRizvi <izhar28husain@gmail.com>
2022-06-07 13:39:03 +0530

    state render file

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -16,10 +16,12 @@ class AppStrings {
   static const String skip = "Skip";
   static const String username = "Username";
   static const String usernameError = "Username is wrong";
+  static const String passwordError = "password is wrong";
   static const String password = "Password";
-  static const String passwordError = "Password is wrong";
   static const String login = "Login";
-  static const String forgetPassword = "Forget Password";
+  static const String forgetPassword = "Forget password";
   static const String registerText = "Not a member ? Sign up";
-
+  static const String loading = "Loading...";
+  static const String retry_again = "Retry Again";
+  static const String ok = "Ok";
 }
